c35475a90a2a94bb477e87529911079e7d91c58b
danju4rizzl <danjumashiwaju@gmail.com>
2024-04-15 14:10:20 +0200

    setup import aliasing vite + typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
+/*
+ ! Make sure to pnpm install -D @types/node in to avoid issues with importing path and using __dirname
+*/
 export default defineConfig({
   plugins: [react()],
   resolve: {
